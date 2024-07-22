b444198569e073df9529915fabd1fbabdcf5c3ab
haroon <skharoon21@gmail.com>
2024-07-22 23:37:59 +0530

    complete UI

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,76 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import {
+  FlatList,
+  Image,
+  ListRenderItem,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React from "react";
+import data from "@/assets/data.json";
+import { Ionicons } from "@expo/vector-icons";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-
-export default function HomeScreen() {
+const TabOneScreen = () => {
+  const RenderItem: ListRenderItem<any> = ({ item }) => {
+    return (
+      <View style={styles.cartItemContainer}>
+        <Image style={styles.cartImage} source={{ uri: item?.image }} />
+        <View style={styles.itemContainer}>
+          <Text style={styles.cartItemName}>{item?.title}</Text>
+          <Text style={{}}>Price: ${item?.price}</Text>
+        </View>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={{ padding: 10 }}>
+            <Ionicons name="remove" size={20} color={"#000"} />
+          </TouchableOpacity>
+          <TouchableOpacity style={{ padding: 10 }}>
+            <Ionicons name="add" size={20} color={"#000"} />
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  };
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <View style={styles.container}>
+      <Text style={styles.title}>TabOneScreen</Text>
+      <FlatList data={data} renderItem={RenderItem} />
+    </View>
   );
-}
+};
+
+export default TabOneScreen;
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  container: {
+    flex: 1,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  title: {
+    fontSize: 20,
+    color: "#FFF",
+    fontWeight: "bold",
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: "80%",
+  },
+  cartItemContainer: {
+    marginBottom: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 20,
+  },
+  cartImage: {
+    height: 50,
+    width: 50,
+  },
+  itemContainer: {
+    flex: 1,
+  },
+  cartItemName: {},
+  buttonContainer: {
+    flexDirection: "row",
+    alignItems: "center",
   },
 });
